dbaeed9ee1c119788b3501c78f0017345e82d391
Albertti <145263297+AlberttiCaleffo@users.noreply.github.com>
2024-03-14 21:26:09 -0300

    Aula de HTML e CSS

--- Aula1403/media.py
+++ Aula1403/media.py
@@ -1,9 +1,8 @@
-def media(n1, n2, n3):
-    media = (n1 + n2 + n3) / 3
-    print(f'{media:.1f}')
+from funcs import media, somaloki
 
 n1 = int(input('Digite o primeiro numero: '))
 n2 = int(input('Digite o segundo numero: '))
 n3 = int(input('Digite o terceiro numero: '))
 
 media(n1, n2, n3)
+somaloki(n1, n2, n3)
