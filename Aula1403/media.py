656c1170d2f52ddf0cc1d8bec947a4ae4a91c596
Albertti <145263297+AlberttiCaleffo@users.noreply.github.com>
2024-03-14 21:04:09 -0300

    Create media.py

---
+++ Aula1403/media.py
@@ -0,0 +1,9 @@
+def media(n1, n2, n3):
+    media = (n1 + n2 + n3) / 3
+    print(f'{media:.1f}')
+
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+n3 = int(input('Digite o terceiro numero: '))
+
+media(n1, n2, n3)
