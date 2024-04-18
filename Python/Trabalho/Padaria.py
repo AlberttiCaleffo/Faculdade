b61e5f021d1f073425a7f202b0af1c9e32c2fa40
Albertti <145263297+AlberttiCaleffo@users.noreply.github.com>
2024-04-18 20:34:02 -0300

    Trabalho

--- Python/Trabalho/Padaria.py
+++ Python/Trabalho/Padaria.py
@@ -9,18 +9,25 @@ print('Bem-Vindo a padaria [nome]')
 sleep(1)
 
 while True:
-    lista_de_compras[inserir_produto()] = inserir_valor()
+    produto = inserir_produto()
+    valor = inserir_valor()
+    lista_de_compras[produto] = valor
     print('Adicionando...')
     sleep(1)
     print('Adicionado')
     print('-+' * 10)
-    total += lista_de_compras[inserir_produto()]
+    total += valor
     for produto, valor in lista_de_compras.items():
         print(f'- {produto:<16}{codigo}\n  {valor:.2f}\n')
-        print(f'TOTAL: {total:.2f}')
         codigo += 1
+    print(f'TOTAL: {total:.2f}')
     print('-+' * 10)
     codigo = 1
-    continuar = input('Encerrar venda? [S/N]: ').strip().lower()
+    continuar = input('Continuar? [S/N]: ').strip().lower()
     if continuar[0] == 'n':
         break 
+    
+for produto, valor in lista_de_compras.items():
+    print(f'- {produto:<16}{codigo}\n  {valor:.2f}\n')
+    codigo += 1
+print(f'TOTAL: {total:.2f}')
