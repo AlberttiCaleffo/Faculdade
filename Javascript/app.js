942eae707644f233a4d1b4585f886cfa79307661
Albertti <145263297+AlberttiCaleffo@users.noreply.github.com>
2024-08-29 21:25:09 -0300

    Update app.js

--- Javascript/app.js
+++ Javascript/app.js
@@ -13,14 +13,27 @@
 
 // console.log(resultado);
 
-let valor_1 = Number(prompt("Primeiro valor: "));
-let valor_2 = Number(prompt("Segundo valor: "));
-let vezes = 0;
-let contador = 0;
+// let valor_1 = Number(prompt("Primeiro valor: "));
+// let valor_2 = Number(prompt("Segundo valor: "));
+// let vezes = 0;
+// let contador = 0;
 
-for(let i = 0; vezes <= valor_1; i++){
-    vezes += valor_2;
-    contador = i;
-}
+// for(let i = 0; vezes <= valor_1; i++){
+//     vezes += valor_2;
+//     contador = i;
+// }
 
-console.log(contador);
+// console.log(contador);
+
+// let i = 20;
+
+// while(i <= 10){
+//     document.write(`${i} </br>`);
+//     i++
+// }
+
+let j = 10;
+
+do{
+    console.log(j);
+}while(j != 10);
